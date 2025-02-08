bfc805f40336755e565b611350079afaccf4041d
mjhcc365 <2209011911@qq.com>
2025-02-08 16:34:02 +0800

    add demo from codepen

--- app/modules/Main/index.tsx
+++ app/modules/Main/index.tsx
@@ -2,11 +2,19 @@ import { ReactLenis, useLenis } from "lenis/react";
 import gsap from "gsap";
 import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+// import { ScrollSmoother } from "gsap/dist/ScrollSmoother";
 
 import { useStore } from "@/app/hooks/useStore";
 
 gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollSmoother);
 ScrollTrigger.defaults({ markers: true });
+
+let smoother = ScrollSmoother.create({
+  smooth: 2,
+  effects: true,
+  normalizeScroll: true,
+});
 
 function App() {
   const lenis = useLenis();
