6a2a08dbea155b44ae81c83b3614c32eecad44be
mjhcc365 <2209011911@qq.com>
2025-08-01 16:49:16 +0800

    style: 添加svg logo

--- app/pages/Header/index.tsx
+++ app/pages/Header/index.tsx
@@ -1,17 +1,20 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="border-b border-stone-800 bg-stone-900/80 backdrop-blur-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex flex-col justify-between items-start h-auto py-4 gap-4">
-          <div className="flex flex-col items-start space-y-2">
-            <h1 className="text-xl font-bold ">Animate Student</h1>
-            <span className="text-xs text-stone-400 bg-stone-800 px-2 py-1 rounded-full self-start">
+        <div className="flex w-full  h-auto py-4 gap-4">
+          <div className="flex w-full justify-between items-center space-x-3">
+            <Image
+              src="/logo.svg"
+              alt="Animate Student Logo"
+              width={32}
+              height={32}
+              className="h-8 w-auto"
+            />
+            <span className="text-xs text-stone-400 bg-stone-800 px-2 py-1 rounded-full">
               网站特效案例库
-            </span>
-          </div>
-          <div className="flex items-center">
-            <span className="text-xs text-stone-400 text-center">
-              收集和展示现代网站动画效果
             </span>
           </div>
         </div>
