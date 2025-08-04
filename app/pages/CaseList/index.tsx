c43edf2c832f5de93015752778f147e77c515ece
mjhcc365 <2209011911@qq.com>
2025-08-04 09:54:30 +0800

    style: 添加响应式效果

--- app/pages/CaseList/index.tsx
+++ app/pages/CaseList/index.tsx
@@ -16,14 +16,16 @@ interface CaseListProps {
 const CaseList = (props: CaseListProps) => {
   const { caseArray = [] } = props;
   return (
-    <div className="space-y-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 lg:grid-cols-1">
       {caseArray.map((demo: CaseType) => (
         <Link
           key={demo.path}
           href={demo.path}
-          className="border border-stone-800 flex flex-col p-4 gap-4 rounded-xl"
+          className="border border-stone-800 flex flex-col lg:flex-row p-4 gap-4 rounded-xl"
         >
-          <div className="border border-stone-800">image</div>
+          <div className="border min-h-16 border-stone-800 lg:order-1 lg:min-w-36 rounded-s ">
+            image
+          </div>
           <div className="flex-shrink-0">
             <span className="border border-stone-800 text-xs font-medium text-slate-300 bg-stone-700 px-3 py-1 rounded">
               NEW RESOURCE
@@ -36,20 +38,20 @@ const CaseList = (props: CaseListProps) => {
                 <h4 className="text-lg font-bold text-white group-hover:text-blue-400 transition-colors truncate">
                   {demo.name}
                 </h4>
-                <div className="flex items-center gap-2 mt-1">
-                  <span className="text-xs text-stone-400">CATEGORY</span>
-                  <span className="text-sm font-semibold text-white">
-                    {demo.category}
-                  </span>
-                </div>
+                <p className="text-sm text-stone-400 line-clamp-2">
+                  {demo.description}
+                </p>
               </div>
               <div className="text-xs text-stone-500 ml-4 flex-shrink-0">
                 {demo.date}
               </div>
             </div>
-            <p className="text-sm text-stone-400 line-clamp-2">
-              {demo.description}
-            </p>
+            <div className="flex items-center gap-2 mt-1">
+              <span className="text-xs text-stone-400">CATEGORY</span>
+              <span className="text-sm font-semibold text-white">
+                {demo.category}
+              </span>
+            </div>
           </div>
         </Link>
       ))}
--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["mew.xyz", "www.datocms-assets.com"], // 在这里添加你的域名
   },
-  // webpack: (config) => {
-  //   // 添加对 GLSL 文件的支持
-  //   config.module.rules.push({
-  //     test: /\.(glsl|vs|fs|vert|frag)$/,
-  //     exclude: /node_modules/,
-  //     use: ["raw-loader"],
-  //   });
-
-  //   return config;
-  // },
   experimental: {
     turbo: {
       rules: {
