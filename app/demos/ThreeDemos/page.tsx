c7dffb06410a2760b1b1c6d94a7698ebb8eaf912
mjhcc365 <2209011911@qq.com>
2025-08-07 10:47:19 +0800

    添加camera  helper

--- app/demos/ThreeDemos/page.tsx
+++ app/demos/ThreeDemos/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as THREE from "three";
-import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
   OrbitControls,
   TransformControls,
@@ -10,6 +10,7 @@ import {
   Text,
   Float,
   MeshReflectorMaterial,
+  useHelper,
 } from "@react-three/drei";
 import { useRef, useEffect, useMemo } from "react";
 import { useControls } from "leva";
@@ -18,8 +19,6 @@ import { Perf } from "r3f-perf";
 import "./page.css";
 
 const verticesCount = 50;
-
-// extend({ OrbitControls });
 
 const Cube = () => {
   const ref = useRef<THREE.Mesh>(null!);
@@ -34,23 +33,21 @@ const Cube = () => {
   //   // ref.current.rotation.y += delta * 0.5;
   // });
 
-  // const positions = useMemo(() => {
-  //   const positions = new Float32Array(verticesCount * 3);
+  const positions = useMemo(() => {
+    const positions = new Float32Array(verticesCount * 3);
 
-  //   for (let i = 0; i < verticesCount * 3; i++)
-  //     positions[i] = (Math.random() - 0.5) * 3;
+    for (let i = 0; i < verticesCount * 3; i++)
+      positions[i] = (Math.random() - 0.5) * 3;
 
-  //   return positions;
-  // }, []);
+    return positions;
+  }, []);
 
-  // useEffect(() => {
-  //   geometryRef.current.computeVertexNormals();
-  // }, [positions]);
+  useEffect(() => {
+    geometryRef.current.computeVertexNormals();
+  }, [positions]);
 
   return (
     <>
-      {/* @ts-ignore */}
-      {/* <orbitControls args={[camera, gl.domElement]} /> */}
       <OrbitControls />
       <TransformControls position={[-2, 0, 0]} mode="translate">
         <mesh>
@@ -62,9 +59,7 @@ const Cube = () => {
   );
 };
 
-const ThreeDemos = () => {
-  const ref = useRef<THREE.Mesh>(null!);
-
+const DemoBaisc = () => {
   return (
     <Canvas
       dpr={[1, 2]}
@@ -72,18 +67,18 @@ const ThreeDemos = () => {
       camera={{ position: [0, 3, 6] }}
     >
       <Perf position="top-left" />
-      {/* <ambientLight intensity={1.5} /> */}
+      <ambientLight intensity={1.5} />
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
-      {/* <pointLight position={[10, 10, 0]} /> */}
+      <pointLight position={[10, 10, 0]} />
       <Cube />
-      {/* <mesh>
+      <mesh>
         <torusKnotGeometry />
         <meshNormalMaterial />
       </mesh>
       <mesh position={[4, 0, 0]}>
         <sphereGeometry />
         <meshBasicMaterial color="mediumpurple" wireframe />
-      </mesh> */}
+      </mesh>
       <PivotControls
         anchor={[0, 0, 0]}
         lineWidth={4}
@@ -116,6 +111,56 @@ const ThreeDemos = () => {
           222222222
         </Text>
       </Float>
+    </Canvas>
+  );
+};
+
+const Environment = () => {
+  const directionalLightRef = useRef<THREE.DirectionalLight>(null!);
+
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1);
+
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight intensity={1.5} />
+      <directionalLight
+        ref={directionalLightRef}
+        position={[1, 1, 3]}
+        intensity={4.5}
+      />
+      <color args={["ivory"]} attach="background" />
+      <mesh position={[2, 0, 0]} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position={[-2, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh position={[0, -1, 0]} scale={10} rotation-x={-Math.PI / 2}>
+        <planeGeometry />
+        <meshStandardMaterial color="lightgreen" side={THREE.DoubleSide} />
+      </mesh>
+    </>
+  );
+};
+
+const ThreeDemos = () => {
+  const ref = useRef<THREE.Mesh>(null!);
+
+  // useEffect(() => {
+  //   directionalLightRef.current.position.set(1, 2, 3);
+  // }, []);
+
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      gl={{ antialias: true }}
+      camera={{ position: [0, 3, 6] }}
+    >
+      <Environment />
     </Canvas>
   );
 };
